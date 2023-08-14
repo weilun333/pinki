0cfc186b80adf227100c710c915199aa34ed656c
wei <holalion@gmail.com>
2023-08-14 19:24:10 +0800

    init commit

---
+++ src/main/java/com/cw/pinki/PinkiApplication.java
@@ -0,0 +1,13 @@
+package com.cw.pinki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cw/pinki/PinkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cw.pinki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
