8d1990fd5f5e53b012f913eb03f0d0b3a3b952e1
wei <holalion@gmail.com>
2023-09-13 00:16:42 +0800

    feat: 新增會員註冊,登入功能
    調整項目：
    1. Member日期型別改為LocalDate, LocalDateTime。

--- src/main/java/com/cw/pinki/common/vo/Member.java
+++ src/main/java/com/cw/pinki/common/vo/Member.java
@@ -1,10 +1,23 @@
 package com.cw.pinki.common.vo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-import java.util.Date;
+import com.cw.pinki.common.annotation.ValidPassword;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,16 +31,22 @@ public class Member {
     @Column(name = "member_no")
     private Integer memberNo;
 
+    @NotBlank(message = "會員姓名不得為空")
     @Column(name = "name")
     private String name;
 
+    @Past(message = "生日不可為未來日期")
     @Column(name = "birthday")
     @JsonFormat(pattern = "yyyy-MM-dd")
-    private java.sql.Date birthday;
+    private LocalDate  birthday;
 
+    @NotBlank(message = "會員信箱不得為空")
+    @Pattern(regexp = "^\\w{1,63}@[a-zA-Z0-9]{2,63}\\.[a-zA-Z]{2,63}(\\.[a-zA-Z]{2,63})?$", message = "不符合e-mail格式，請確認")
     @Column(name = "mail")
     private String mail;
 
+    @ValidPassword
+    @NotBlank(message = "密碼不得為空")
     @Column(name = "member_password")
     private String memberPassword;
 
@@ -51,12 +70,13 @@ public class Member {
 
     @Column(name = "create_time")
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "GMT+8")
-    private Date createTime;
+    private LocalDateTime createTime;
 
     @Column(name = "process_user")
     private String processUser;
 
     @Column(name = "last_update_time")
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "GMT+8")
-    private Date lastUpdateTime;
+    private LocalDateTime lastUpdateTime;
+	
 }
